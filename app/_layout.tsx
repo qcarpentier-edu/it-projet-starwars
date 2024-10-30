007d7226bc65e10887d0c519ed39323931cab2db
Quentin Carpentier <quentin.carpentier@condorcet.be>
2024-10-30 13:35:05 +0100

    Ajout de la police StarJedi (avec expo-font et le hook useFonts)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
 import { Stack } from "expo-router";
 import { UserProfileProvider } from './context/UserProfileContext';
+import { ActivityIndicator } from "react-native";
+import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
+  // Charger la police StarJedi avec le hook useFonts (téléchargée et mise dans ../assets/fonts)
+  const [fontsLoaded] = useFonts({
+    'Starjedi': require('../assets/fonts/Starjedi.ttf'),
+  });
+
+  // Afficher un chargement uniquement si la police n'est pas chargée
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+  
   return (
     // Rendre disponible le fournisseur aux autres écrans (Provider) 
     <UserProfileProvider>
